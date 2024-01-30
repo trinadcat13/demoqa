4ac1a0ffac8a9773c23242a65e1db6cb3cfbdc22
Nika <miishka_93@list.ru>
2024-01-30 18:58:24 +0300

    Проект с урока

---
+++ tests/test_check_icon_demoqa.py
@@ -0,0 +1,7 @@
+from pages.demoqa import DemoQa
+def test_check_icon(browser):
+    demo_qa_page = DemoQa(browser)
+    demo_qa_page.visit()
+    demo_qa_page.click_on_the_icon()
+    assert demo_qa_page.equal_url()
+    assert demo_qa_page.exist_icon()
